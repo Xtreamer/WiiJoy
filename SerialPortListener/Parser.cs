7b957ff096d7a9c4f60513039874dad30c40aaca
Jonas <jonas.feldt@gmail.com>
2015-12-05 23:58:36 +0100

    Filter bad values

--- SerialPortListener/Parser.cs
+++ SerialPortListener/Parser.cs
@@ -11,12 +11,14 @@ namespace SerialPortListener
         //private bool _quit = false;
         private const int bufferSize = 200;
         private const int payloadLength = 16;
-        private const int headerLength = 6;
+        private const int headerLength = 5;
         private byte[] buffer;
         //private int position;
         private byte[] responsePrefix;
         private short numberOfChannels = 4;
         private Stopwatch stopwatch;
+        private long numberOfPackets = 0;
+        private long badPackets = 0;
 
         public short NumberOfChannels
         {
@@ -37,12 +39,14 @@ namespace SerialPortListener
         
         public void NewSerialDataReceived(object sender, SerialDataEventArgs e)
         {
+            //numberOfPackets++;
             //Console.WriteLine("Elapsed {0}", stopwatch.ElapsedMilliseconds);
             var elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
             stopwatch.Restart();
             
             if (e.Length == 0)
             {
+                //badPackets++;
                 return;
             }
 
@@ -55,11 +59,18 @@ namespace SerialPortListener
             }
 
             var indexBefore = startIndex;
-            if (startIndex + (payloadLength + headerLength) > e.Length)
+            var totalLength = numberOfChannels * 2 + headerLength;
+            if (startIndex + totalLength > e.Length)
             {
                 startIndex = Array.IndexOf(data, responsePrefix[0]);
                 //startIndex = startIndex - (payloadLength + headerLength);
-                Console.WriteLine("before {0}, now {1}, length {2}", indexBefore, startIndex, e.Length);
+                //Console.WriteLine("before {0}, now {1}, length {2}", indexBefore, startIndex, e.Length);
+                if (indexBefore == startIndex)
+                {
+                    //badPackets++;
+                    //Console.WriteLine("badPackets: {0} %", ((float)badPackets / numberOfPackets) * 100);
+                    return;
+                }
             }
             //if (startIndex + 1 < e.Length && data[startIndex + 1] != responsePrefix[1])
             //{
@@ -70,20 +81,20 @@ namespace SerialPortListener
             //    Console.WriteLine("good, index now: {0}, index before: {1}, length: {2}, left: {3}", startIndex, indexBefore, e.Length, e.Length - startIndex + 1);
             //Console.WriteLine("handle, length: {0}, firstIndex {1}, charAtFirst {2}", e.Length, startIndex, data[startIndex]);
 
-            if (data.Length > 15 &&
-                data[0] == responsePrefix[0] &&
-                data[1] == responsePrefix[1] &&
-                data[2] == responsePrefix[2])
+            if (data.Length >= totalLength + startIndex &&
+                data[startIndex] == responsePrefix[0] &&
+                data[startIndex + 1] == responsePrefix[1] &&
+                data[startIndex + 2] == responsePrefix[2])
             {
-                var roll = GetPositionInteger(data, 5);
-                var pitch = GetPositionInteger(data, 5 + 2);
-                var yaw = GetPositionInteger(data, 5 + 4);
-                var throttle = GetPositionInteger(data, 5 + 6);
-                var aux1 = GetPositionInteger(data, 5 + 8);
+                var roll = GetPositionInteger(data, startIndex + 5);
+                var pitch = GetPositionInteger(data, startIndex + 5 + 2);
+                var yaw = GetPositionInteger(data, startIndex + 5 + 4);
+                var throttle = GetPositionInteger(data, startIndex + 5 + 6);
+                //var aux1 = GetPositionInteger(data, startIndex + 5 + 8);                
                 //Console.WriteLine("{0}, {1}, {2}, {3}", roll, pitch, yaw, throttle);
 
                 if (NewPositionDataRecieved != null)
-                    NewPositionDataRecieved(this, new PositionDataEventArgs(roll, pitch, yaw, throttle, aux1, elapsedMilliseconds));
+                    NewPositionDataRecieved(this, new PositionDataEventArgs(roll, pitch, yaw, throttle, 0, elapsedMilliseconds));
             }
         }
 
