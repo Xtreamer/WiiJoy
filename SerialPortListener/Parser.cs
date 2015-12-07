66f986397a2a3634b05774471bf1d6638056dfb1
Jonas <jonas.feldt@gmail.com>
2015-12-08 00:42:07 +0100

    Cleanup

--- SerialPortListener/Parser.cs
+++ SerialPortListener/Parser.cs
@@ -7,32 +7,23 @@ namespace SerialPortListener
 {
     public class SerialPortDataParser
     {
-        //Thread worker;
-        //private bool _quit = false;
         private const int bufferSize = 200;
         private const int payloadLength = 16;
-        private const int headerLength = 5;
+        private int headerLength;
         private byte[] buffer;
-        //private int position;
-        private byte[] responsePrefix;
-        private short numberOfChannels = 4;
+        private byte[] responsePrefix = new byte[] { (byte)'$', (byte)'M', (byte)'>', 16, 105 };
         private Stopwatch stopwatch;
         private long numberOfPackets = 0;
         private long badPackets = 0;
 
-        public short NumberOfChannels
-        {
-            get { return numberOfChannels; }
-            set { numberOfChannels = value; }
-        }
-
+        public short NumberOfChannels { get; set; } = 4;
+        
         public event EventHandler<PositionDataEventArgs> NewPositionDataRecieved;
 
         public SerialPortDataParser()
         {
             buffer = new byte[bufferSize];
-            responsePrefix = new byte[] { (byte)'$', (byte)'M', (byte)'>', 16, 105 };
-            //position = 0;
+            headerLength = responsePrefix.Length;
             stopwatch = new Stopwatch();
             stopwatch.Start();
         }
@@ -59,12 +50,10 @@ namespace SerialPortListener
             }
 
             var indexBefore = startIndex;
-            var totalLength = numberOfChannels * 2 + headerLength;
+            var totalLength = NumberOfChannels * 2 + headerLength;
             if (startIndex + totalLength > e.Length)
             {
                 startIndex = Array.IndexOf(data, responsePrefix[0]);
-                //startIndex = startIndex - (payloadLength + headerLength);
-                //Console.WriteLine("before {0}, now {1}, length {2}", indexBefore, startIndex, e.Length);
                 if (indexBefore == startIndex)
                 {
                     //badPackets++;
@@ -72,25 +61,18 @@ namespace SerialPortListener
                     return;
                 }
             }
-            //if (startIndex + 1 < e.Length && data[startIndex + 1] != responsePrefix[1])
-            //{
-            //    Console.WriteLine("not complete {0}", DateTime.Now);
-            //    return;
-            //}
-            //if (startIndex != indexBefore)
-            //    Console.WriteLine("good, index now: {0}, index before: {1}, length: {2}, left: {3}", startIndex, indexBefore, e.Length, e.Length - startIndex + 1);
-            //Console.WriteLine("handle, length: {0}, firstIndex {1}, charAtFirst {2}", e.Length, startIndex, data[startIndex]);
-
+            
             if (data.Length >= totalLength + startIndex &&
                 data[startIndex] == responsePrefix[0] &&
                 data[startIndex + 1] == responsePrefix[1] &&
                 data[startIndex + 2] == responsePrefix[2])
             {
-                var roll = GetPositionInteger(data, startIndex + 5);
-                var pitch = GetPositionInteger(data, startIndex + 5 + 2);
-                var yaw = GetPositionInteger(data, startIndex + 5 + 4);
-                var throttle = GetPositionInteger(data, startIndex + 5 + 6);
-                //var aux1 = GetPositionInteger(data, startIndex + 5 + 8);                
+                var payloadStartIndex = startIndex + headerLength;
+                var roll = GetPositionInteger(data, payloadStartIndex);
+                var pitch = GetPositionInteger(data, payloadStartIndex + 2);
+                var yaw = GetPositionInteger(data, payloadStartIndex + 4);
+                var throttle = GetPositionInteger(data, payloadStartIndex + 6);
+                //var aux1 = GetPositionInteger(data, startIndex + 5 + 8);
                 //Console.WriteLine("{0}, {1}, {2}, {3}", roll, pitch, yaw, throttle);
 
                 if (NewPositionDataRecieved != null)
